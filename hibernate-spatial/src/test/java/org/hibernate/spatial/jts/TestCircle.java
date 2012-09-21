38ac8f594979ab56c5330a3da1475fa9bcb65737
Karel Maesen <karel@geovise.com>
2012-09-21 21:09:56 +0200

    Moves the Circle class to the jts package.

--- hibernate-spatial/src/test/java/org/hibernate/spatial/jts/TestCircle.java
+++ hibernate-spatial/src/test/java/org/hibernate/spatial/jts/TestCircle.java
@@ -18,9 +18,10 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.hibernate.spatial;
+package org.hibernate.spatial.jts;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import org.hibernate.spatial.jts.Circle;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
